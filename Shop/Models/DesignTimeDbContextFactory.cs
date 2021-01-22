e64d83549bfd2d22a5df2b8ace41e21a46d973b9
Austin Schrader <austin.jschrader@gmail.com>
2021-01-21 22:56:37 -0800

    Add designtimedb method to designtimedbcontext

--- Shop/Models/DesignTimeDbContextFactory.cs
+++ Shop/Models/DesignTimeDbContextFactory.cs
@@ -5,5 +5,21 @@ using System.IO;
 
 namespace Shop.Models
 {
+  public class ShopContextFactory : IDesignTimeDbContextFactory<ShopContext>
+  {
+    ShopContext IDesignTimeDbContextFactory<ShopContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build()
 
+      var builder = new DbContextOptionsBuilder<ShopContext>();
+      var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+      builder.UseMySql(connectionString);
+
+      return new ShopContext(builder.Options);
+    }
+  }
 }
