f93eb70237600b623580c21a6f10c00de91d71ef
Austin Schrader <austin.jschrader@gmail.com>
2021-01-21 23:19:42 -0800

    Add password and confirm password to register model

--- Shop/ViewModels/RegisterViewModel.cs
+++ Shop/ViewModels/RegisterViewModel.cs
@@ -9,6 +9,14 @@ namespace Shop.ViewModels
     [Display(Name = "Email")]
     public string Email { get; set; }
 
+    [Required]
+    [DataType(DataType.Password)]
+    [Display(Name = "Password")]
+    public string Password { get; set; }
 
+    [DataType(DataType.Password)]
+    [Display(Name = "Confirm Password")]
+    [Compare("Password", ErrorMessage = "The two passwords don't match.")]
+    public string ConfirmPassword { get; set; }
   }
 }
